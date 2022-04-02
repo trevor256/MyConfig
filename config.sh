b8ce3f3c546d24c31b6d15145404143d63e9aa14
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 <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>
2022-04-02 16:28:31 -0500

    added formating and fixed a script

--- config.sh
+++ config.sh
@@ -1,26 +1,33 @@
-#!/bin/bash/
+#!/bin/sh
 #######################################
-# Bash script to config a new system (KDE NEON)
+# Bash script to config a new linux system (KDE NEON)
 #######################################
 
-echo '###Update..'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+Yellow='\033[0;33m'
+NC='\033[0m' # No Color
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+echo "${GREEN}${bold}#Update..${NC}${normal}"
 sudo pkcon update -y
 
-echo '###changing theme..'
-lookandfeeltool -a org.kde.breezedark.desktop
+echo "${GREEN}${bold}#changing theme..${NC}${normal}"
+sudo lookandfeeltool -a org.kde.breezedark.desktop
 
-echo '###Installing xp-pen drivers..'
-curl https://www.xp-pen.com/download/file/id/1949/pid/819/ext/deb.html -o xpen.deb
+echo "${GREEN}${bold}#Installing xp-pen drivers..${NC}${normal}"
+sudo curl https://www.xp-pen.com/download/file/id/1949/pid/819/ext/deb.html -o xpen.deb
 sudo dpkg -i xpen.deb
 
-echo '###Installing Rust..'
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+echo "${GREEN}${bold}#Installing Rust..${NC}${normal}"
+sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-echo '###Installing Node & NPM..'
+echo "${GREEN}${bold}#Installing Node & NPM..${NC}${normal}"
 sudo apt install nodejs npm -y
 
-echo '###Installing Atom, kdenlive, & krita..'
-sudo apt-get install kdenlive krita -y
+echo "${GREEN}${bold}#Installing Atom, kdenlive, krita, & Docker..${NC}${normal}"
+sudo apt-get install kdenlive krita docker-ce docker-ce-cli containerd.io -y
 
-echo '###Installing blender, Inkscape, Blankets, easyssh, gnomeBoxes, godotengine..'
-flatpak install flathub org.blender.Blender org.inkscape.Inkscape com.rafaelmardojai.Blanket com.github.muriloventuroso.easyssh org.gnome.Boxes org.godotengine.Godot io.atom.Atom -y
+echo "${GREEN}${bold}#Installing blender, Inkscape, Blankets, easyssh, gnomeBoxes, godotengine..${NC}${normal}"
+sudo flatpak install flathub org.blender.Blender org.inkscape.Inkscape com.rafaelmardojai.Blanket com.github.muriloventuroso.easyssh org.gnome.Boxes org.godotengine.Godot io.atom.Atom -y
