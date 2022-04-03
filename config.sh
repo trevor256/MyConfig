bf9386986cf4b00916ee0b4243e5cf5cb8a63978
Trevor <79394682+trevor256@users.noreply.github.com>
2022-04-03 17:18:21 -0500

    Update config.sh

--- config.sh
+++ config.sh
@@ -14,9 +14,6 @@ echo "${GREEN}${bold}##Update..${NC}${normal}"
 sudo pkcon update -y
 sudo flatpak update -y
 
-echo "${GREEN}${bold}##changing theme..${NC}${normal}"
-lookandfeeltool -a org.kde.breezedark.desktop
-
 echo "${GREEN}${bold}##Downloading xp-pen drivers..${NC}${normal}"
 sudo curl https://www.xp-pen.com/download/file/id/1949/pid/819/ext/deb.html -o xpen.deb
 echo "${GREEN}${bold}##Installing xp-pen drivers..${NC}${normal}"
@@ -24,21 +21,21 @@ sudo dpkg -i xpen.deb
 
 echo "${GREEN}${bold}##Installing Rust..${NC}${normal}"
 sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+echo 1
 
-echo "${GREEN}${bold}##Installing Node 17, NPM..${NC}${normal}"
+echo "${GREEN}${bold}##Installing Node 17 & NPM..${NC}${normal}"
 curl -sL https://deb.nodesource.com/setup_current.x | sudo -E bash -
-sudo apt install nodejs -y
+sudo apt-get install -y nodejs
 curl -qL https://www.npmjs.com/install.sh | sh
-sudo apt install npm -y
 
-echo "${GREEN}${bold}##Installing  NPM, kdenlive, krita, & Docker..${NC}${normal}"
-sudo apt-get install npm kdenlive krita docker -y
+echo "${GREEN}${bold}##Installing kdenlive, krita, & Docker..${NC}${normal}"
+sudo apt-get install kdenlive krita docker -y
 
 echo "${GREEN}${bold}##Installing blender, Inkscape, Blankets, easyssh, gnomeBoxes, godotengine..${NC}${normal}"
 sudo flatpak install flathub org.blender.Blender org.inkscape.Inkscape com.rafaelmardojai.Blanket com.github.muriloventuroso.easyssh org.gnome.Boxes org.godotengine.Godot io.atom.Atom -y
 
-echo "${GREEN}${bold}##Installing nvidia-driver-510..${NC}${normal}"
-sudo apt install nvidia-driver-510 -y
+echo "${GREEN}${bold}##changing theme..${NC}${normal}"
+lookandfeeltool -a org.kde.breezedark.desktop
 
 echo "${GREEN}${bold}##downloading github projects to github dir..${NC}${normal}"
 mkdir github
@@ -47,7 +44,18 @@ git clone https://github.com/trevor256/LinuxConfig.git
 git clone https://github.com/trevor256/FileCodify.git
 git clone https://github.com/trevor256/rusty.git
 
-echo "${GREEN}${bold}##Update & removing xpen.deb..${NC}${normal}"
+#Install NVIDIA Driver?
+   echo -n "${RED}${bold}Install NVIDIA Driver?${NC}${normal} (y/n)"
+   read reply
+
+   if [ "$reply" = y -o "$reply" = Y ]
+   then
+      sudo apt install nvidia-driver-510 -y
+   else
+      echo "${RED}${bold}NVIDIA Driver not installed${NC}${normal}"
+   fi
+
+echo "${GREEN}${bold}##Finishing up..${NC}${normal}"
 sudo rm xpen.deb
 sudo pkcon update -y
 sudo flatpak update -y
