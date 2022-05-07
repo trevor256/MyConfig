d26b4bb0e7cdaca2c9b46e7f2067b42917b9df30
Trevor <79394682+trevor256@users.noreply.github.com>
2022-05-06 21:43:42 -0500

    Update config.sh

--- config.sh
+++ config.sh
@@ -16,31 +16,31 @@ sudo pkcon update -y
 sudo flatpak update -y
 
 echo "${GREEN}${bold} Installing GO..${NC}${normal}"
-sudo curl -OL https://go.dev/dl/go1.18.1.linux-amd64.tar.gz
+curl -OL https://go.dev/dl/go1.18.1.linux-amd64.tar.gz
 sudo tar -C /usr/local -xvf go1.18.1.linux-amd64.tar.gz
 sudo echo 'export PATH=$PATH:/usr/local/go/bin' >>~/.profile
-sudo source ~/.profile
+source ~/.profile
 
 echo "${GREEN}${bold} Installing Rust..${NC}${normal}"
-sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 sudo apt-get install rustc cargo -y
 
 echo "${GREEN}${bold} Installing Node 17 & NPM yarn..${NC}${normal}"
-sudo curl -sL https://deb.nodesource.com/setup_current.x | -E bash -
+curl -sL https://deb.nodesource.com/setup_current.x | -E bash -
 sudo apt-get install -y nodejs
-sudo curl -qL https://www.npmjs.com/install.sh | sh
+sudo curl -qL https://www.npmjs.com/install.sh | sudo sh
 sudo npm install -g nodemon
 sudo npm install --global yarn
 
 echo "${GREEN}${bold} Installing AWS CLI..${NC}${normal}"
-sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-sudo unzip awscliv2.zip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
 sudo ./aws/install
 
 echo "${GREEN}${bold} Installing GCP CLI..${NC}${normal}"
 sudo apt-get install apt-transport-https ca-certificates gnupg -y
-sudo echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-sudo curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+sudo curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 sudo apt-get update -y && apt-get install google-cloud-cli -y
 
 echo "${GREEN}${bold} Installing blender, Inkscape, easyssh, gnomeBoxes, godotengine, videodownloader, Nixwriter, kdenlive, krita, Docker, ffmpeg, wine32, marktext..${NC}${normal}"
@@ -58,31 +58,31 @@ git clone https://github.com/trevor256/trevdev-token github/trevdev-token/
 chmod -R 777 github
 
 echo "${GREEN}${bold} Install Xpen drivers..${NC}${normal}"
-sudo curl https://www.xp-pen.com/download/file/id/1949/pid/819/ext/deb.html -o xpen.deb
+curl https://www.xp-pen.com/download/file/id/1949/pid/819/ext/deb.html -o xpen.deb
 sudo dpkg -i xpen.deb
 
 echo "${GREEN}${bold} Install NVIDIA drivers?${NC}${normal} (y/n)"
 read reply
  if [ "$reply" = y -o "$reply" = Y ]
    then
-      sudo sudo ubuntu-drivers autoinstall
+      sudo ubuntu-drivers autoinstall
    else
       echo "${RED}${bold} Drivers not installed${NC}${normal}"
    fi
 
 echo "${GREEN}${bold} Finishing up..${NC}${normal}"
-rm xpen.deb
+lookandfeeltool -a 'org.kde.breezedark.desktop'
+sudo rm xpen.deb
 sudo pkcon update -y
-sudo flatpak update -y
 
-    echo -n "${RED}${bold}  remove config.sh script and reboot?${NC}${normal} (y/n)"
-    read reply
-    if [ "$reply" = y -o "$reply" = Y ]
-    then
+echo -n "${RED}${bold}  Remove config.sh script and reboot?${NC}${normal} (y/n)"
+read reply
+  if [ "$reply" = y -o "$reply" = Y ]
+   then
       #setting display settings to 2560x1440 164Hz..
         xrandr --output DP-2 --mode 2560x1440 --rate 164
         rm -- "$0"
         reboot
     else
-       echo "${RED}${bold}  stopped reboot and config.sh script not removed${NC}${normal}"
+       echo "${RED}${bold}  Stopped reboot and config.sh script not removed${NC}${normal}"
     fi
