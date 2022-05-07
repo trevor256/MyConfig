dd9f95a6931414bc0e434d2fbbb50d2ab7f8628c
Trevor <79394682+trevor256@users.noreply.github.com>
2022-05-06 21:30:00 -0500

    Update config.sh

--- config.sh
+++ config.sh
@@ -11,40 +11,41 @@ bold=$(tput bold)
 normal=$(tput sgr0)
 
 echo "${GREEN}${bold} Updating..${NC}${normal}"
-dpkg --add-architecture i386
-pkcon update -y
+sudo dpkg --add-architecture i386
+sudo pkcon update -y
+sudo flatpak update -y
 
 echo "${GREEN}${bold} Installing GO..${NC}${normal}"
-curl -OL https://go.dev/dl/go1.18.1.linux-amd64.tar.gz
+sudo curl -OL https://go.dev/dl/go1.18.1.linux-amd64.tar.gz
 sudo tar -C /usr/local -xvf go1.18.1.linux-amd64.tar.gz
-echo 'export PATH=$PATH:/usr/local/go/bin' >>~/.profile
-source ~/.profile
+sudo echo 'export PATH=$PATH:/usr/local/go/bin' >>~/.profile
+sudo source ~/.profile
 
 echo "${GREEN}${bold} Installing Rust..${NC}${normal}"
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-apt-get install rustc cargo -y
+sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+sudo apt-get install rustc cargo -y
 
 echo "${GREEN}${bold} Installing Node 17 & NPM yarn..${NC}${normal}"
-curl -sL https://deb.nodesource.com/setup_current.x | -E bash -
-apt-get install -y nodejs
-curl -qL https://www.npmjs.com/install.sh | sh
-npm install -g nodemon
-npm install --global yarn
+sudo curl -sL https://deb.nodesource.com/setup_current.x | -E bash -
+sudo apt-get install -y nodejs
+sudo curl -qL https://www.npmjs.com/install.sh | sh
+sudo npm install -g nodemon
+sudo npm install --global yarn
 
 echo "${GREEN}${bold} Installing AWS CLI..${NC}${normal}"
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-./aws/install
+sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo unzip awscliv2.zip
+sudo ./aws/install
 
 echo "${GREEN}${bold} Installing GCP CLI..${NC}${normal}"
-apt-get install apt-transport-https ca-certificates gnupg -y
-echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-apt-get update -y && apt-get install google-cloud-cli -y
+sudo apt-get install apt-transport-https ca-certificates gnupg -y
+sudo echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+sudo curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+sudo apt-get update -y && apt-get install google-cloud-cli -y
 
 echo "${GREEN}${bold} Installing blender, Inkscape, easyssh, gnomeBoxes, godotengine, videodownloader, Nixwriter, kdenlive, krita, Docker, ffmpeg, wine32, marktext..${NC}${normal}"
-flatpak install flathub org.blender.Blender org.inkscape.Inkscape com.github.muriloventuroso.easyssh org.gnome.Boxes org.godotengine.Godot io.atom.Atom com.github.unrud.VideoDownloader com.gitlab.adnan338.Nixwriter com.github.marktext.marktext -y
-apt-get install kdenlive krita docker ffmpeg wine32 build-essential slimbookbattery -y
+sudo flatpak install flathub org.blender.Blender org.inkscape.Inkscape com.github.muriloventuroso.easyssh org.gnome.Boxes org.godotengine.Godot io.atom.Atom com.github.unrud.VideoDownloader com.gitlab.adnan338.Nixwriter com.github.marktext.marktext -y
+sudo apt-get install kdenlive krita docker ffmpeg wine32 build-essential slimbookbattery -y
 
 echo "${GREEN}${bold} Downloading github projects to github dir..${NC}${normal}"
 git clone https://github.com/trevor256/trevor256.com.git github/trevor256.com/
@@ -57,22 +58,22 @@ git clone https://github.com/trevor256/trevdev-token github/trevdev-token/
 chmod -R 777 github
 
 echo "${GREEN}${bold} Install Xpen drivers..${NC}${normal}"
-curl https://www.xp-pen.com/download/file/id/1949/pid/819/ext/deb.html -o xpen.deb
-dpkg -i xpen.deb
+sudo curl https://www.xp-pen.com/download/file/id/1949/pid/819/ext/deb.html -o xpen.deb
+sudo dpkg -i xpen.deb
 
 echo "${GREEN}${bold} Install NVIDIA drivers?${NC}${normal} (y/n)"
 read reply
  if [ "$reply" = y -o "$reply" = Y ]
    then
-      sudo ubuntu-drivers autoinstall
+      sudo sudo ubuntu-drivers autoinstall
    else
       echo "${RED}${bold} Drivers not installed${NC}${normal}"
    fi
 
 echo "${GREEN}${bold} Finishing up..${NC}${normal}"
 rm xpen.deb
-pkcon update -y
-flatpak update -y
+sudo pkcon update -y
+sudo flatpak update -y
 
     echo -n "${RED}${bold}  remove config.sh script and reboot?${NC}${normal} (y/n)"
     read reply
