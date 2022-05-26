222094e742b5583a36ac5d3e409b370a7d9b5e60
Trevor <79394682+trevor256@users.noreply.github.com>
2022-05-26 08:20:04 -0500

    Update config.sh

--- config.sh
+++ config.sh
@@ -61,7 +61,7 @@ chmod -R 777 github
 echo "${GREEN}${bold} Install Xpen and other drivers..${NC}${normal}"
 curl https://www.xp-pen.com/download/file/id/1949/pid/819/ext/deb.html -o xpen.deb
 sudo dpkg -i xpen.deb
-sudo ubuntu-drivers autoinstall
+sudo apt-install nvidia-driver-510 
 
 echo "${GREEN}${bold} Finishing up..${NC}${normal}"
 lookandfeeltool -a 'org.kde.breezedark.desktop'
