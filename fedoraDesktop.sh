5d558fc629e98bbd2b9027a7bb41eae6ad85b0a2
Trevor256 <79394682+trevor256@users.noreply.github.com>
2022-06-25 18:30:25 -0500

    Create fedoraDesktop.sh

---
+++ fedoraDesktop.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+########################################
+#  Author https://github.com/trevor256
+#  script configs new system
+#  Linux OS (KDE NEON)
+########################################
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+echo "${GREEN}${bold} Updating..${NC}${normal}"
+sudo dnf upgrade --refresh -y
+
+echo "${GREEN}${bold} Installing go rust cargo nodejs awscli google-cloud-cli azure-cli..${NC}${normal}"
+sudo dnf -y install go rust cargo nodejs awscli google-cloud-cli azure-cli
+        
+echo "${GREEN}${bold} Installing kdenlive, ktorrent, krita, ffmpeg, build-essential, nmap, moc, moc-ffmpeg-plugin, vim, nvim, ktorrent
+blender, libreoffice, discord, Inkscape, gnomeBoxes, godotengine, videodownloader, bootqt, libllvm6.0, Audacity, OBS, marktext..${NC}${normal}"
+sudo dnf install ffmpeg build-essential nmap moc moc-ffmpeg-plugin -y
+sudo flatpak install flathub org.kde.ktorrent org.vim.Vim org.blender.Blender org.libreoffice.LibreOffice com.discordapp.Discord io.neovim.nvim com.github.marktext.marktext org.kde.kdenlive org.inkscape.Inkscape org.kde.krita org.gnome.Boxes com.valvesoftware.Steam org.godotengine.Godot com.github.unrud.VideoDownloader io.github.giantpinkrobots.bootqt org.audacityteam.Audacity com.obsproject.Studio -y
+
+echo "${GREEN}${bold} Downloading github projects to github dir..${NC}${normal}"
+git config --global user.name "trevor256"
+git config --global user.email "256trevor@gmail.com"
+git clone https://github.com/trevor256/trevor256.com.git github/trevor256.com/
+git clone https://github.com/trevor256/LinuxConfig.git github/MyConfig/
+git clone https://github.com/trevor256/FileCodify.git github/FileCodify/
+git clone https://github.com/trevor256/erupt-0.2.git github/erupt-0.2/
+git clone https://github.com/trevor256/AutoBlender.git github/AutoBlender/
+git clone https://github.com/trevor256/trevdev-token github/trevdev-token/
+git clone https://github.com/trevor256/MinecraftServer.git github/MinecraftServer/
+chmod -R 777 github
+
+echo "${GREEN}${bold} Install Xpen${NC}${normal}"
+curl https://www.xp-pen.com/download/file/id/1948/pid/819/ext/rpm.html -o xpen.rpm
+sudo dpkg -i xpen.rpm
+sudo rm xpen.rpm
+
+echo "${GREEN}${bold} install nvidia driver?${NC}${normal} (y/n)"
+read -r reply
+  if [ "$reply" = y ] || [ "$reply" = Y ]
+   then
+      sudo apt-get install nvidia-driver-510 -y
+    else
+       echo "${RED}${bold}  nvidia driver not installed${NC}${normal}"
+    fi
+
+echo "${GREEN}${bold} rebooting......${NC}${normal}"
+sudo pkcon update -y
+sudo reboot
+    
