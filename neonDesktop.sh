1cfa366bc6fe151eb18cac4d5eb264275ee54fff
Trevor256 <79394682+trevor256@users.noreply.github.com>
2022-07-06 22:18:12 -0500

    Update neonDesktop.sh

--- neonDesktop.sh
+++ neonDesktop.sh
@@ -44,7 +44,7 @@ curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
         
 echo "${GREEN}${bold} Installing kdenlive, krita, ffmpeg, build-essential, nmap, moc, moc-ffmpeg-plugin, nvim, ktorrent
 blender, libreoffice, discord, Inkscape, gnomeBoxes, godotengine, videodownloader, libllvm6.0, Audacity, OBS, marktext, minecraft..${NC}${normal}"
-sudo apt-get install ffmpeg build-essential nmap moc moc-ffmpeg-plugin -y
+sudo apt-get install ffmpeg build-essential nmap moc moc-ffmpeg-plugin virt-manager-y
 sudo flatpak install flathub org.kde.ktorrent org.blender.Blender org.libreoffice.LibreOffice com.discordapp.Discord io.neovim.nvim com.github.marktext.marktext org.kde.kdenlive org.inkscape.Inkscape org.kde.krita org.gnome.Boxes org.godotengine.Godot com.github.unrud.VideoDownloader org.audacityteam.Audacity com.mojang.Minecraft com.obsproject.Studio -y
 
 echo "${GREEN}${bold} Monero CLI..${NC}${normal}"
