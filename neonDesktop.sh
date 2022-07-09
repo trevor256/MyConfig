7d77bfbdf0915f445c902605141468e27a9aea9f
Trevor256 <79394682+trevor256@users.noreply.github.com>
2022-07-09 12:48:31 -0500

    Update neonDesktop.sh

--- neonDesktop.sh
+++ neonDesktop.sh
@@ -70,6 +70,17 @@ sudo dpkg -i xpen.deb
 sudo rm xpen.deb go1.18.1.linux-amd64.tar.gz awscliv2.zip
 lookandfeeltool -a 'org.kde.breezedark.desktop'
 
+echo "${GREEN}${bold} Installing custom commands, makeboot${NC}${normal}"
+# run "source .bashrc" to load anything added to .bashrc
+
+printf "function makeboot() {
+  wget -nc https://files.kde.org/neon/images/user/20220707-0946/neon-user-20220707-0946.iso -O Downloads/neon.iso
+  sudo umount /dev/sda 
+  sudo dd bs=4M if=Downloads/neon.iso of=/dev/sda conv=fdatasync  status=progress
+}
+export -f makeboot" >> .bashrc
+source .bashrc
+
 echo "${GREEN}${bold} install nvidia driver?${NC}${normal} (y/n)"
 read -r reply
   if [ "$reply" = y ] || [ "$reply" = Y ]
