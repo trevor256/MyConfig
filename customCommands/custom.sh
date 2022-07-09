830c094797933ac5c04121d5aeab0d1170da8966
Trevor256 <79394682+trevor256@users.noreply.github.com>
2022-07-09 12:33:13 -0500

    Update custom.sh

--- customCommands/custom.sh
+++ customCommands/custom.sh
@@ -1,8 +1,7 @@
 # run "source .bashrc" to load anything added to .bashrc
 
 function makeboot() {
-  wget https://files.kde.org/neon/images/user/20220707-0946/neon-user-20220707-0946.iso -O Downloads/neon.iso
-  lsblk
+  wget -nc https://files.kde.org/neon/images/user/20220707-0946/neon-user-20220707-0946.iso -O Downloads/neon.iso
   sudo umount /dev/sda 
   sudo dd bs=4M if=Downloads/neon.iso of=/dev/sda conv=fdatasync  status=progress
 }
