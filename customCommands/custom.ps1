370ec594e1d2e0f9499d77199e137eb7d03a2bf6
Trevor256 <79394682+trevor256@users.noreply.github.com>
2022-07-16 17:26:01 -0500

    Delete customCommands directory

--- customCommands/custom.ps1
+++
@@ -1,8 +0,0 @@
-# \Windows\System32\WindowsPowerShell\v1.0\Modules. Now, create a folder within which your module will reside. The folder name must match the module name. For example, I am calling my module Demo, so my folder will also be named Demo.
-
-Function testing
-{
-Write-Host 'this is a test'
-}
-
-Export-ModuleMember -Function ‘testing’
--- customCommands/custom.sh
+++
@@ -1,8 +0,0 @@
-# run "source .bashrc" to load anything added to .bashrc
-
-function makeboot() {
-  wget -nc https://files.kde.org/neon/images/user/20220707-0946/neon-user-20220707-0946.iso -O Downloads/neon.iso
-  sudo umount /dev/sda 
-  sudo dd bs=4M if=Downloads/neon.iso of=/dev/sda conv=fdatasync  status=progress
-}
-export -f makeboot
